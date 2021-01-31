d19ff059f42d5da5808d2009e8ad9c835b67bc75
Thiwanon Chromcharoen <gmgame.1525@gmail.com>
2021-01-31 07:06:21 +0700

    Apply

--- Assets/Scripts/StartStory.cs
+++ Assets/Scripts/StartStory.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Febucci.UI;
+using TMPro;
 using UnityEngine;
 
 public class StartStory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public TextMeshProUGUI textDialog;
+
+    private TextAnimator _textAnimator;
+
+    private void Start()
     {
-        
+        _textAnimator = textDialog.GetComponent<TextAnimator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadNextScene()
     {
-        
+        Initiate.Fade("Chapter 1", Color.black, 1f);
+    }
+
+    public void ChangeText(string text)
+    {
+        _textAnimator.SyncText(text, false);
     }
 }
